33c8fd29a3c1345ebd8514e0ad15012a4b3ad9f4
ycm666 <ycm666@naver.com>
2018-03-30 22:25:10 +0900

    Daily Delete 추가

---
+++ src/action/daily/DailyDeleteAction.java
@@ -0,0 +1,60 @@
+package action.daily;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URLDecoder;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
+import dao.DailyDao;
+import vo.DailyVo;
+
+/**
+ * Servlet implementation class DialyInsertAction
+ */
+@WebServlet("/daily/delete.do")
+public class DailyDeleteAction extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		int d_index = Integer.parseInt(request.getParameter("d_index"));
+		
+		DailyVo  vo = DailyDao.getInstance().selectOne(d_index);
+		
+		String save_dir = request.getServletContext().getRealPath("/upload/");
+		//첨부화일이 있으면 화일삭제
+		if(vo.getD_image().equals("no_file")) {
+			File f = new File(save_dir,vo.getD_image());
+			f.delete();
+		}
+		//DB삭제
+		int res = DailyDao.getInstance().delete(d_index);
+		
+		//성공여부
+		boolean bSuccess = true;
+		
+		if(res==0) bSuccess = false;
+		
+		//회원가입결과를 : {'success': true} 형식으로 전송
+		JSONObject json = new JSONObject();
+		json.put("success", bSuccess);
+		response.setContentType("text/plain; charset=utf-8");
+		response.getWriter().print(json.toJSONString());
+
+	}
+
+}
--- src/dao/DailyDao.java
+++ src/dao/DailyDao.java
@@ -128,4 +128,100 @@ public class DailyDao {
 		return res;
 	}
 
+	public DailyVo selectOne(int d_index) {
+		// TODO Auto-generated method stub
+		
+		DailyVo vo = null;
+		String sql = "select * from daily where d_index=?";
+
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			//1.Connection 얻어오기
+			conn = DBService.getInstance().getConnection();
+			//2.명령처리객체 얻어오기
+			pstmt = conn.prepareStatement(sql);
+
+			//2-1.pstmt setting
+			pstmt.setInt(1, d_index);
+			     
+			//3.결과행처리객체 얻어오기
+			rs = pstmt.executeQuery();
+			//처음~끝까지 반복해라..
+			if (rs.next()) {
+				//1개의 레코드를 저장할 객체
+				vo = new DailyVo();
+
+				//현재레코드(rs)의 필드값을 얻어와서->VO에 넣는다
+				//vo에 넣기
+				vo.setD_index(rs.getInt("d_index"));
+				vo.setD_subject(rs.getString("d_subject"));
+				vo.setD_content(rs.getString("d_content"));
+				vo.setD_date(rs.getString("d_date"));
+				vo.setD_image(rs.getString("d_image"));
+				vo.setG_index(rs.getInt("g_index"));
+				
+			
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally {
+
+			try {
+				//열린 역순으로 닫기
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return vo;
+	}
+
+	public int delete(int d_index) {
+		// TODO Auto-generated method stub
+		int res = 0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = "delete from daily where d_index=?";
+
+		try {
+			//1.Connection얻기
+			conn = DBService.getInstance().getConnection();
+			//2.명령처리객체 얻기
+			pstmt = conn.prepareStatement(sql);
+			//3.pstmt의 parameter setting
+			pstmt.setInt(1, d_index);
+			//4.DB 전송			
+			res = pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally {
+
+			try {
+				//열린 역순으로 닫기
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return res;
+	}
+	
+
 }
