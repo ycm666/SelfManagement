93d68b991d465f1fb6fc89d9be84f4a9471bdb7a
ycm666 <ycm666@naver.com>
2018-03-31 21:43:25 +0900

    GroupList삭제

---
+++ src/action/grouplist/GroupListDeleteAction.java
@@ -0,0 +1,68 @@
+package action.grouplist;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
+
+import dao.DailyDao;
+import dao.GroupListDao;
+import vo.DailyVo;
+
+/**
+ * Servlet implementation class DialyInsertAction
+ */
+@WebServlet("/grouplist/delete.do")
+public class GroupListDeleteAction extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		int g_index = Integer.parseInt(request.getParameter("g_index"));
+		
+		List<DailyVo> daily_list = DailyDao.getInstance().selectList(g_index);
+		
+		String save_dir = request.getServletContext().getRealPath("/upload/");
+		int res = 0;
+		for(DailyVo vo : daily_list) {
+			//첨부화일이 있으면 화일삭제
+			if(vo.getD_image().equals("no_file")) {
+				File f = new File(save_dir,vo.getD_image());
+				f.delete();
+			}
+			//DB삭제
+			res = res *  DailyDao.getInstance().delete(vo.getD_index());
+		}
+		
+		res = res * GroupListDao.getInstance().delete(g_index);
+		
+		
+		//성공여부
+		boolean bSuccess = true;
+		
+		if(res==0) bSuccess = false;
+		
+		//회원가입결과를 : {'success': true} 형식으로 전송
+		JSONObject json = new JSONObject();
+		json.put("success", bSuccess);
+		response.setContentType("text/plain; charset=utf-8");
+		response.getWriter().print(json.toJSONString());
+
+	}
+
+}
--- src/dao/GroupListDao.java
+++ src/dao/GroupListDao.java
@@ -124,4 +124,40 @@ public class GroupListDao {
 		}
 		return res;
 	}
+
+	public int delete(int g_index) {
+		// TODO Auto-generated method stub
+		int res = 0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = "delete from group_list where g_index=?";
+
+		try {
+			//1.Connection얻기
+			conn = DBService.getInstance().getConnection();
+			//2.명령처리객체 얻기
+			pstmt = conn.prepareStatement(sql);
+			//3.pstmt의 parameter setting
+			pstmt.setInt(1, g_index);
+			//4.DB 전송			
+			res = pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally {
+
+			try {
+				//열린 역순으로 닫기
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return res;
+	}
 }
